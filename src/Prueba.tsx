ecb09b5b04bfa161d9ad2a2cea80fe0463b461cd
Stiven Trujillo Ortiz <strujillo1293@gmail.com>
2020-07-24 10:23:26 -0500

    React and Styled Components with TypeScript Examples

--- src/Prueba.tsx
+++ src/Prueba.tsx
@@ -1,5 +1,62 @@
-import React from 'react';
+import React, { useState} from 'react';
 import styled, { css } from 'styled-components';
+
+// REACT WITH TYPESCRIPT
+
+// Escribir de manera más corta este tipo de descripciones
+type FormElement = React.FormEvent<HTMLFormElement>;
+
+// Definimos la forma de una Task
+interface ITask {
+    name: string,
+    done: boolean
+}
+
+// Explicación genéricos en interfaces
+// interface React.FC<T> {
+//     props: T
+//     ... 
+// }
+export const AppPrueba: React.FC<{ message: string }> = ({ message }) => {
+    
+    //                     Estamos recibiendo un string
+    const [newTask, setNewTask] = useState<string>('');
+    //                     Estamos recibiendo un Arreglo de ITask
+    const [tasks, setTasks] = useState<ITask[]>([]);
+
+    // FormEvent to onSubmit
+    const handleSubmit = (e : FormElement) => {
+        e.preventDefault();
+        console.log('enviando...' + newTask);
+        addTask( newTask );
+        setNewTask( '' );
+    }
+
+    const addTask = (name: string) => {
+        // newTasks es un arreglo de ITask s
+        const newTasks: ITask[] = [...tasks, { name, done: false }];
+        setTasks(newTasks);
+    }
+
+    // ChangeEvent to onChange
+    const handleChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+        setNewTask( e.target.value );
+    }
+
+    return (
+        <>
+            <h1>{ message }</h1>
+            <form onSubmit={ handleSubmit }>
+                <input type="text" onChange={ handleChange } value={ newTask } />
+                <input type="submit" value="Save"/>
+            </form>
+        </>
+    )
+}
+
+// ___
+
+// STYLED COMPONENTS
 
 // Ejemplo código css dinámico 
 const flex = css`
@@ -31,6 +88,12 @@ const Button = styled.button<ButtonProps>`
     background-color: ${ props => props.bg || '' };
     /* border: none; */
 `;
+// También se puede hacer de la siguiente forma:
+const Button2 = styled.button<ButtonProps>(props => `
+    padding: 1em 2.5em;
+    margin: 1em;
+    background-color: ${ props.bg || '' };
+`)
 
 interface Props {
     message: string
