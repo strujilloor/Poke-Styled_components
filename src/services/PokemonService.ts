6a94601d8eea774422885fffb714e1f650505dca
Stiven Trujillo Ortiz <36318021+strujilloor@users.noreply.github.com>
2021-11-27 00:22:48 -0500

    Add space between types

--- src/services/PokemonService.ts
+++ src/services/PokemonService.ts
@@ -14,7 +14,7 @@ export const getPokemonByName = async (name: string) : Promise<IPokemon> => {
         image: data.sprites.front_default,  // stores front pokemon image
         name: data.name,                    // stores pokemon name
         details: data.types.length > 1      // stores pokemon types name
-            ? `${data.types[0].type.name} ${data.types[1].type.name}`
+            ? `${data.types[0].type.name}   ${data.types[1].type.name}`
             : data.types[0].type.name
     }
     return pokemon;
